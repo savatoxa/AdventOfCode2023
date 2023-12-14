a5954f1a96b5c05a291e7d2100e61294a8f4d708
savatoxa <savatoxa@yandex.ru>
2023-12-15 00:17:17 +0300

    errors

--- Day3.cs
+++ Day3.cs
@@ -7,25 +7,57 @@ using System.Threading.Tasks;
 public class Scanner3
 {
     Parser parser = new Parser();
+    List<List<char>> numSymMatrix = new List<List<char>>();
+
     public List<List<char>> GetMatrix(string data)
     {
         return parser.GetCharMatrix(data);
     }
+    public List<Tuple<int, int>> Radar(int x, int y)
+    {
+        List<Tuple<int, int>> adjacent = new List<Tuple<int, int>>();
+        if (char.IsDigit(numSymMatrix[x + 1][y]))
+            adjacent.Add(Tuple.Create(x + 1, y));
+        if (char.IsDigit(numSymMatrix[x + 1][y - 1]))
+            adjacent.Add(Tuple.Create(x + 1, y - 1));
+        if (char.IsDigit(numSymMatrix[x][y - 1]))
+            adjacent.Add(Tuple.Create(x, y - 1));
+        if (char.IsDigit(numSymMatrix[x - 1][y - 1]))
+            adjacent.Add(Tuple.Create(x - 1, y - 1));
+        if (char.IsDigit(numSymMatrix[x - 1][y]))
+            adjacent.Add(Tuple.Create(x - 1, y));
+        if (char.IsDigit(numSymMatrix[x - 1][y + 1]))
+            adjacent.Add(Tuple.Create(x - 1, y + 1));
+        if (char.IsDigit(numSymMatrix[x][y + 1]))
+            adjacent.Add(Tuple.Create(x, y + 1));
+        if (char.IsDigit(numSymMatrix[x + 1][y + 1]))
+            adjacent.Add(Tuple.Create(x + 1, y + 1));
+        return adjacent;
+    }
     public int SumPartNumbers(string data)
     {
-        var numSymMatrix = GetMatrix(data);
+        numSymMatrix = GetMatrix(data);
+        List<Tuple<int, int>> symbolscoords = new List<Tuple<int, int>>();
         parser.PrintMatrix(numSymMatrix);
         int sum = 0;
 
         for (int j = 0; j < numSymMatrix.Count - 1; j++)
             for (int i = 0; i < numSymMatrix[j].Count - 1; i++)
-            {
-                var numsym = numSymMatrix[j][i];
-                if (!char.IsDigit(numsym) && numsym != '.')
-                {
-                    Console.WriteLine("{0}, {1}, {2}", numsym, i, j);
-                }
-            }
+                if (!char.IsDigit(numSymMatrix[j][i]) && numSymMatrix[j][i] != '.')
+                    symbolscoords.Add(Tuple.Create(i,j));
+
+        foreach (var coord in symbolscoords)
+        {
+            Console.WriteLine(coord);
+            Console.WriteLine(numSymMatrix[coord.Item2][coord.Item1]);
+        }
+        Console.WriteLine("___");
+        var adj = Radar(symbolscoords[0].Item2, symbolscoords[0].Item1);
+        foreach (var p in adj)
+        {
+            Console.WriteLine(p);
+            Console.WriteLine(numSymMatrix[p.Item1][p.Item2]);
+        }
 
         return sum;
     }
