c164a5d3d4c33008d411d6776e7359fcafc45149
savatoxa <savatoxa@yandex.ru>
2023-12-15 17:05:06 +0300

    Radar works;

--- Day3.cs
+++ Day3.cs
@@ -7,56 +7,57 @@ using System.Threading.Tasks;
 public class Scanner3
 {
     Parser parser = new Parser();
-    List<List<char>> numSymMatrix = new List<List<char>>();
+    List<List<char>> matrix = new List<List<char>>();
 
     public List<List<char>> GetMatrix(string data)
     {
         return parser.GetCharMatrix(data);
     }
-    public List<Tuple<int, int>> Radar(int x, int y)
+    public List<Tuple<int, int>> Radar(int row, int col)
     {
         List<Tuple<int, int>> adjacent = new List<Tuple<int, int>>();
-        if (char.IsDigit(numSymMatrix[x + 1][y]))
-            adjacent.Add(Tuple.Create(x + 1, y));
-        if (char.IsDigit(numSymMatrix[x + 1][y - 1]))
-            adjacent.Add(Tuple.Create(x + 1, y - 1));
-        if (char.IsDigit(numSymMatrix[x][y - 1]))
-            adjacent.Add(Tuple.Create(x, y - 1));
-        if (char.IsDigit(numSymMatrix[x - 1][y - 1]))
-            adjacent.Add(Tuple.Create(x - 1, y - 1));
-        if (char.IsDigit(numSymMatrix[x - 1][y]))
-            adjacent.Add(Tuple.Create(x - 1, y));
-        if (char.IsDigit(numSymMatrix[x - 1][y + 1]))
-            adjacent.Add(Tuple.Create(x - 1, y + 1));
-        if (char.IsDigit(numSymMatrix[x][y + 1]))
-            adjacent.Add(Tuple.Create(x, y + 1));
-        if (char.IsDigit(numSymMatrix[x + 1][y + 1]))
-            adjacent.Add(Tuple.Create(x + 1, y + 1));
+        if (char.IsDigit(matrix[row][col+1]))
+            adjacent.Add(Tuple.Create(row, col+1));
+        if (char.IsDigit(matrix[row-1][col+1]))
+            adjacent.Add(Tuple.Create(row-1, col+1));
+        if (char.IsDigit(matrix[row-1][col]))
+            adjacent.Add(Tuple.Create(row-1, col));
+        if (char.IsDigit(matrix[row-1][col-1]))
+            adjacent.Add(Tuple.Create(row-1, col-1));
+        if (char.IsDigit(matrix[row][col-1]))
+            adjacent.Add(Tuple.Create(row, col-1));
+        if (char.IsDigit(matrix[row+1][col-1]))
+            adjacent.Add(Tuple.Create(row+1, col-1));
+        if (char.IsDigit(matrix[row+1][col]))
+            adjacent.Add(Tuple.Create(row+1, col));
+        if (char.IsDigit(matrix[row+1][col+1]))
+            adjacent.Add(Tuple.Create(row+1, col+1));
         return adjacent;
+    }
+    public int GetNumber(Tuple<int,int> coordinate)
+    {
+        return 0;
     }
     public int SumPartNumbers(string data)
     {
-        numSymMatrix = GetMatrix(data);
+        matrix = GetMatrix(data);
         List<Tuple<int, int>> symbolscoords = new List<Tuple<int, int>>();
-        parser.PrintMatrix(numSymMatrix);
+        parser.PrintMatrix(matrix);
         int sum = 0;
 
-        for (int j = 0; j < numSymMatrix.Count - 1; j++)
-            for (int i = 0; i < numSymMatrix[j].Count - 1; i++)
-                if (!char.IsDigit(numSymMatrix[j][i]) && numSymMatrix[j][i] != '.')
-                    symbolscoords.Add(Tuple.Create(i,j));
+        for (int row = 0; row < matrix.Count - 1; row++)
+            for (int col = 0; col < matrix[row].Count - 1; col++)
+                if (!char.IsDigit(matrix[row][col]) && matrix[row][col] != '.')
+                    symbolscoords.Add(Tuple.Create(row,col));
 
+        Console.WriteLine("___");
         foreach (var coord in symbolscoords)
         {
-            Console.WriteLine(coord);
-            Console.WriteLine(numSymMatrix[coord.Item2][coord.Item1]);
-        }
-        Console.WriteLine("___");
-        var adj = Radar(symbolscoords[0].Item2, symbolscoords[0].Item1);
-        foreach (var p in adj)
-        {
-            Console.WriteLine(p);
-            Console.WriteLine(numSymMatrix[p.Item1][p.Item2]);
+            Console.WriteLine("---");
+            Console.WriteLine("{0}, {1}", matrix[coord.Item1][coord.Item2], coord);
+            var adj = Radar(coord.Item1, coord.Item2);
+            foreach (var p in adj)
+                Console.WriteLine("{0}, {1}", matrix[p.Item1][p.Item2], p);
         }
 
         return sum;
