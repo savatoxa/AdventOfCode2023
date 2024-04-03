6d41772c5874cc64760ef61d36a22c05b13c65b7
savatoxa <savatoxa@yandex.ru>
2024-04-03 17:27:31 +0300

    wip

--- Day5.cs
+++ Day5.cs
@@ -7,11 +7,18 @@ using System.Threading.Tasks;
 public class Scanner5
 {
     List<int> seeds = new List<int>();
+    List<List<int>> seedsToSoil = new List<List<int>>();
     Parser parser = new Parser();
     public void GetSeeds(string data)
     {
-     var seedStrArr =  parser.SplitStrBySpaces2(parser.SplitStrByEmptyLines(data)[0]);
-        foreach (var str_ in seedStrArr)
-            Console.WriteLine(str_);
+        var dataStrArr = parser.SplitStrByEmptyLines(data);
+        var seedStrArr = parser.SplitStrBySpaces2(dataStrArr[0]);
+        var seedsToSoilStrArr = parser.SplitStrByLines(dataStrArr[1]);
+        for (int i = 1; i < seedStrArr.Length; i++)
+            seeds.Add(Int32.Parse(seedStrArr[i]));
+        seeds.ForEach(e => Console.Write(e + " "));
+        Console.WriteLine("");
+        //for (int i = 1; i < seedsToSoilStrArr.Length; i++)
+
     }
 }
