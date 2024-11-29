f4f7c458714d940d9f464242a020ade2b0ebe2d4
savatoxa <savatoxa@yandex.ru>
2024-11-29 23:59:56 +0300

    day8 added

---
+++ Day8.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Scanner8
+{
+
+}
+
+    public void Run(string data)
+    {
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        watch.Stop();
+        var elapsedMs = watch.Elapsed.TotalMilliseconds;
+        Console.WriteLine(elapsedMs);
+    }
+}
