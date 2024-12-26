b0e7229c64ce5592f26bcccb75b589680796ceb7
savatoxa <savatoxa@yandex.ru>
2024-12-26 19:22:38 +0300

    8.2 test variant is ready, but main case works incorrect

--- Day8.cs
+++ Day8.cs
@@ -14,7 +14,7 @@ public class Scanner8
 
     public void CreateMoves(string data)
     {
-        moves =  parser.SplitStrByEmptyLines(data)[0];
+        moves = parser.SplitStrByEmptyLines(data)[0];
     }
     public void CreateMap(string data)
     {
@@ -22,33 +22,49 @@ public class Scanner8
         foreach (var submap in mapsStr)
             map.Add(Tuple.Create(submap.Substring(0, 3), submap.Substring(7, 3), submap.Substring(12, 3)));
     }
-
     public string GetNodeByMove(string node, char move)
     {
-    foreach (var nodes in map)
-        if (nodes.Item1 == node)
-        {
-            if (move == 'R')
-                return nodes.Item3;
-            if (move == 'L')
-                return nodes.Item2;
-        }
+        foreach (var nodes in map)
+            if (nodes.Item1 == node)
+            {
+                if (move == 'R')
+                    return nodes.Item3;
+                if (move == 'L')
+                    return nodes.Item2;
+            }
         return "";
     }
-
     public List<string> GetListOfNodesByMove(List<string> inputnodes, char move)
     {
         List<string> outputnodes = new List<string>();
         foreach (var node in inputnodes)
-        {
-
-        }
+            outputnodes.Add(GetNodeByMove(node, move));
         return outputnodes;
     }
-
+    public bool IsListZ(List<string> inputlist)
+    {
+        foreach (var node in inputlist)
+            if (node[2] != 'Z')
+                return false;
+        return true;
+    }
+    public Tuple<List<string>, int> GetListOfNodesByAllMoves(List<string> inputlist, int count)
+    {
+        //Tuple<List<string>, int> output = new Tuple<List<string>, int>(null, 0);
+        //Tuple<List<string>, int> output = new Tuple<List<string>, int>(new List<string>(), 0);
+        foreach (var move in moves)
+        {
+            inputlist = GetListOfNodesByMove(inputlist, move);
+            count++;
+        }
+        if (IsListZ(inputlist))
+            return Tuple.Create(inputlist, count);
+        else
+            return GetListOfNodesByAllMoves(inputlist, count);
+    }
     public Tuple<string, int> GetNodeByAllMoves(string inputnode, int count)
     {
-        foreach(var move in moves)
+        foreach (var move in moves)
         {
             inputnode = GetNodeByMove(inputnode, move);
             count++;
@@ -58,29 +74,60 @@ public class Scanner8
         else
             return GetNodeByAllMoves(inputnode, count);
     }
-
     public List<string> GetNodesA()
     {
         List<string> nodesC = new List<string>();
-        foreach(var node in map)
+        foreach (var node in map)
         {
             if (node.Item1[2] == 'A')
                 nodesC.Add(node.Item1);
         }
         return nodesC;
     }
-
+    public int GCD(int m, int n)
+    {
+        while(m != n)
+        {
+            if (m > n)
+                m = m - n;
+            else
+                n = n - m;
+        }
+        return n;
+    }
+    public int GCDMyltiplyN(List<int> numlist)
+    {
+        int gcd = GCD(numlist[0], numlist[1]);
+        for (var i = 2; i < numlist.Count; i++)
+            gcd = GCD(gcd, numlist[i]);
+        return gcd;
+    }
+    public long LCMofAllnodesA()
+    {
+        List<int> allSeq = new List<int>();
+        long multAllSeq = 1;
+        foreach (var node in nodesA)
+            allSeq.Add(GetNodeByAllMoves(node, 0).Item2);
+        long gcdAllSeq = GCDMyltiplyN(allSeq);
+        foreach (var num in allSeq)
+            multAllSeq *= num/gcdAllSeq;
+        return multAllSeq;
+    }
     public void Run(string data)
     {
         var watch = System.Diagnostics.Stopwatch.StartNew();
         CreateMoves(data);
         CreateMap(data);
         nodesA = GetNodesA();
-        Console.WriteLine(moves);
-        map.ForEach(m => Console.WriteLine(m));
+        //Console.WriteLine(moves);
+        //map.ForEach(m => Console.WriteLine(m));
         Console.WriteLine("___");
-        //Console.WriteLine(GetNodeByAllMoves("22A", 0));
         nodesA.ForEach(n => Console.WriteLine(n));
+        Console.WriteLine("___");
+        foreach (var node in nodesA)
+            Console.WriteLine(GetNodeByAllMoves(node, 0));
+        Console.WriteLine(LCMofAllnodesA());
+        Console.WriteLine("___");
         watch.Stop();
         var elapsedMs = watch.Elapsed.TotalMilliseconds;
         Console.WriteLine(elapsedMs);
