e8af09dd03ba1304dc7e29fb9a216218abc4c25c
anton.savotkin <anton.savotkin@my.games>
2025-01-11 01:37:51 +0300

    9.2 done

---
+++ Day9.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Scanner9
+{
+    Parser parser = new Parser();
+    List<List<int>> envreport = new List<List<int>>();
+
+    public void CreateEnvreport (string data)
+    {
+        foreach(var strarr in parser.SplitStrByLines(data))
+            envreport.Add(parser.SplitStrBySpaces2(strarr).ToList().Select(s => Convert.ToInt32(s)).ToList());
+    }
+    public List<int> DiffSequense (List<int> sequense)
+    {
+        List<int> diffsequense = new List<int>();
+        for(var i = sequense.Count - 1; i > 0; i --)
+            diffsequense.Insert(0, sequense[i] - sequense[i-1]);
+        return diffsequense;
+    }
+    public bool IfDiffsequenseZero(List<int> sequense)
+    {
+        foreach (var element in sequense)
+            if (element != 0)
+                return false;
+        return true;
+    }
+    public List<List<int>> DiffSequensesList(List<int> sequense)
+    {
+        List<List<int>> diffsequensesList = new List<List<int>>();
+        diffsequensesList.Add(sequense);
+        while(!IfDiffsequenseZero(sequense))
+        {
+            sequense = DiffSequense(sequense);
+            diffsequensesList.Add(sequense);
+        }
+        return diffsequensesList;
+    }
+    public List<List<int>> CompleteDiffSequensesList(List<List<int>> diffsequensesList)
+    {
+        diffsequensesList[diffsequensesList.Count - 1].Insert(0,0);
+        for (var i = diffsequensesList.Count - 1; i > 0; i--)
+            diffsequensesList[i - 1].Insert(0, (diffsequensesList[i - 1][0] - diffsequensesList[i][0]));
+        return diffsequensesList;
+    }
+    public int SumOfLastElementsOfAllCompleteDiffSequensesList()
+    {
+        List<List<int>> completediffsequensesList = new List<List<int>>();
+        int sum = 0;
+        foreach (var l in envreport)
+        {
+            completediffsequensesList = CompleteDiffSequensesList(DiffSequensesList(l));
+            sum += completediffsequensesList[0][0];
+        }
+        return sum;
+    }
+public void Run(string data)
+{
+    var watch = System.Diagnostics.Stopwatch.StartNew();
+    CreateEnvreport(data);
+        Console.WriteLine("___");
+        Console.WriteLine(SumOfLastElementsOfAllCompleteDiffSequensesList());
+    watch.Stop();
+    var elapsedMs = watch.Elapsed.TotalMilliseconds;
+        Console.WriteLine("___");
+    Console.WriteLine(elapsedMs);
+}
+}
--- Program.cs
+++ Program.cs
@@ -27,44 +27,46 @@ class Program
 	{
 		var workingDirectory = DirectoriesMethods.DirUp(Environment.CurrentDirectory, 2);
 
-		//string dataStr1 = File.ReadAllText(workingDirectory + "\\" + "data1.txt");
-		//string dataStr1 = File.ReadAllText(workingDirectory + "\\" + "data1_test_1.txt");
-		//string dataStr1 = File.ReadAllText(workingDirectory + "\\" + "data1_test_2.txt");
-		//string dataStr2 = File.ReadAllText(workingDirectory + "\\" + "data2_test1.txt");
-		//string dataStr2 = File.ReadAllText(workingDirectory + "\\" + "data2.txt");
-		//string dataStr3 = File.ReadAllText(workingDirectory + "\\" + "data3_test1.txt");
-		//string dataStr3 = File.ReadAllText(workingDirectory + "\\" + "data3.txt");
-		//string dataStr4 = File.ReadAllText(workingDirectory + "\\" + "data4_test1.txt");
-		//string dataStr4 = File.ReadAllText(workingDirectory + "\\" + "data4.txt");
-		//string dataStr5 = File.ReadAllText(workingDirectory + "\\" + "data5_test1.txt");
-		//string dataStr5 = File.ReadAllText(workingDirectory + "\\" + "data5.txt");
-		//string dataStr6 = File.ReadAllText(workingDirectory + "\\" + "data6_test1.txt");
-		//string dataStr6 = File.ReadAllText(workingDirectory + "\\" + "data6.txt");
-		//string dataStr7 = File.ReadAllText(workingDirectory + "\\" + "data7.txt");
+        //string dataStr1 = File.ReadAllText(workingDirectory + "\\" + "data1.txt");
+        //string dataStr1 = File.ReadAllText(workingDirectory + "\\" + "data1_test_1.txt");
+        //string dataStr1 = File.ReadAllText(workingDirectory + "\\" + "data1_test_2.txt");
+        //string dataStr2 = File.ReadAllText(workingDirectory + "\\" + "data2_test1.txt");
+        //string dataStr2 = File.ReadAllText(workingDirectory + "\\" + "data2.txt");
+        //string dataStr3 = File.ReadAllText(workingDirectory + "\\" + "data3_test1.txt");
+        //string dataStr3 = File.ReadAllText(workingDirectory + "\\" + "data3.txt");
+        //string dataStr4 = File.ReadAllText(workingDirectory + "\\" + "data4_test1.txt");
+        //string dataStr4 = File.ReadAllText(workingDirectory + "\\" + "data4.txt");
+        //string dataStr5 = File.ReadAllText(workingDirectory + "\\" + "data5_test1.txt");
+        //string dataStr5 = File.ReadAllText(workingDirectory + "\\" + "data5.txt");
+        //string dataStr6 = File.ReadAllText(workingDirectory + "\\" + "data6_test1.txt");
+        //string dataStr6 = File.ReadAllText(workingDirectory + "\\" + "data6.txt");
+        //string dataStr7 = File.ReadAllText(workingDirectory + "\\" + "data7.txt");
         //string dataStr7 = File.ReadAllText(workingDirectory + "\\" + "data7_test1.txt");
-        string dataStr8 = File.ReadAllText(workingDirectory + "\\" + "data8.txt");
+        //string dataStr8 = File.ReadAllText(workingDirectory + "\\" + "data8.txt");
         //string dataStr8 = File.ReadAllText(workingDirectory + "\\" + "data8_test1.txt");
         //string dataStr8 = File.ReadAllText(workingDirectory + "\\" + "data8_test2.txt");
         //string dataStr8 = File.ReadAllText(workingDirectory + "\\" + "data8_test3.txt");
-
+        string dataStr9 = File.ReadAllText(workingDirectory + "\\" + "data9.txt");
+        //string dataStr9 = File.ReadAllText(workingDirectory + "\\" + "data9_test1.txt");
 
 
 
         Parser parser = new Parser();
-		//Scanner1 scanner1 = new Scanner1();
-		//Scanner2 scanner2 = new Scanner2();
-		//Scanner3 scanner3 = new Scanner3();
-		//Scanner4 scanner4 = new Scanner4();
-		//Scanner5 scanner5 = new Scanner5();
-		//Scanner6 scanner6 = new Scanner6();
-		//Scanner7 scanner7 = new Scanner7();
-        Scanner8 scanner8 = new Scanner8();
+        //Scanner1 scanner1 = new Scanner1();
+        //Scanner2 scanner2 = new Scanner2();
+        //Scanner3 scanner3 = new Scanner3();
+        //Scanner4 scanner4 = new Scanner4();
+        //Scanner5 scanner5 = new Scanner5();
+        //Scanner6 scanner6 = new Scanner6();
+        //Scanner7 scanner7 = new Scanner7();
+        //Scanner8 scanner8 = new Scanner8();
+        Scanner9 scanner9 = new Scanner9();
         //Console.WriteLine(scanner1.CalcCalib(dataStr1));
         //Console.WriteLine(scanner2.GetValidGames(dataStr2));
         //Console.WriteLine(scanner3.SumPartNumbers(dataStr3));
         //Console.WriteLine(scanner4.AllCardsNum(dataStr4));
         //scanner5.Run(dataStr5);
         //scanner7.Run(dataStr7);
-        scanner8.Run(dataStr8);
+        scanner9.Run(dataStr9);
     }
 }
