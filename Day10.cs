5f1ef2ab2ccf1f81652386412d9c77e1d7a4e120
anton.savotkin <anton.savotkin@my.games>
2025-03-02 19:17:02 +0300

    GetValidCells updated

--- Day10.cs
+++ Day10.cs
@@ -12,7 +12,6 @@ public class Scanner10
     public void CreatePipemap(string data)
     {
         pipemap = parser.GetCharMatrix(data);
-        parser.PrintMatrix(pipemap);
     }
     public Tuple<int, int> GetStart ()
     {
@@ -24,14 +23,15 @@ public class Scanner10
     }
     public List<Tuple<int, int>> GetValidCells(Tuple<int, int> inputCell)
     {
-        var upperCellChar = ',';
-        var upperRightCellChar = ',';
-        var rightCellChar = ',';
-        var lowerRightCellChar = ',';
-        var lowerCellChar = ',';
-        var lowerLeftCellChar = ',';
-        var leftCellChar = ',';
-        var upperLeftCellChar = ',';
+        var outputcells = new List<Tuple<int, int>>();
+        var upperCellChar = 'x';
+        var upperRightCellChar = 'x';
+        var rightCellChar = 'x';
+        var lowerRightCellChar = 'x';
+        var lowerCellChar = 'x';
+        var lowerLeftCellChar = 'x';
+        var leftCellChar = 'x';
+        var upperLeftCellChar = 'x';
 
         if (inputCell.Item1 > 0)        
             upperCellChar = pipemap[inputCell.Item1 - 1][inputCell.Item2];
@@ -53,11 +53,53 @@ public class Scanner10
         switch (pipemap[inputCell.Item1][inputCell.Item2])
         {
             case 'S':
-                Console.WriteLine("Case: "+'S');
-
+                Console.WriteLine("Case: " + 'S');
+                if (upperCellChar == '|' || upperCellChar == '7' || upperCellChar == 'F')
+                    outputcells.Add(Tuple.Create(inputCell.Item1 - 1, inputCell.Item2));
+                if (rightCellChar == 'J' || rightCellChar == '-' || rightCellChar == '7')
+                    outputcells.Add(Tuple.Create(inputCell.Item1, inputCell.Item2 + 1));
+                if (lowerCellChar == '|' || lowerCellChar == 'J' || lowerCellChar == 'L')
+                    outputcells.Add(Tuple.Create(inputCell.Item1 + 1, inputCell.Item2));
+                if (leftCellChar == '-' || leftCellChar == 'F' || leftCellChar == 'L')
+                    outputcells.Add(Tuple.Create(inputCell.Item1, inputCell.Item2 - 1));
                 break;
             case '.':
-                Console.WriteLine("Case: "+'.');
+                Console.WriteLine("Case: " + '.');
+                break;
+            case 'F':
+                Console.WriteLine("Case: " + 'F');
+                if (rightCellChar == 'J' || rightCellChar == '-' || rightCellChar == '7')
+                    outputcells.Add(Tuple.Create(inputCell.Item1, inputCell.Item2 + 1));
+                if (lowerCellChar == '|' || lowerCellChar == 'J' || lowerCellChar == 'L')
+                    outputcells.Add(Tuple.Create(inputCell.Item1 + 1, inputCell.Item2));
+                break;
+            case '7':
+                Console.WriteLine("Case: " + '7');
+                if (lowerCellChar == '|' || lowerCellChar == 'J' || lowerCellChar == 'L')
+                    outputcells.Add(Tuple.Create(inputCell.Item1 + 1, inputCell.Item2));
+                if (leftCellChar == '-' || leftCellChar == 'F' || leftCellChar == 'L')
+                    outputcells.Add(Tuple.Create(inputCell.Item1, inputCell.Item2 - 1));
+                break;
+            case 'J':
+                Console.WriteLine("Case: " + 'J');
+                if (upperCellChar == '|' || upperCellChar == '7' || upperCellChar == 'F')
+                    outputcells.Add(Tuple.Create(inputCell.Item1 - 1, inputCell.Item2));
+                if (leftCellChar == '-' || leftCellChar == 'F' || leftCellChar == 'L')
+                    outputcells.Add(Tuple.Create(inputCell.Item1, inputCell.Item2 - 1));
+                break;
+            case '-':
+                Console.WriteLine("Case: " + '-');
+                if (leftCellChar == '-' || leftCellChar == 'F' || leftCellChar == 'L')
+                    outputcells.Add(Tuple.Create(inputCell.Item1, inputCell.Item2 - 1));
+                if (rightCellChar == 'J' || rightCellChar == '-' || rightCellChar == '7')
+                    outputcells.Add(Tuple.Create(inputCell.Item1, inputCell.Item2 + 1));
+                break;
+            case '|':
+                Console.WriteLine("Case: " + '|');
+                if (upperCellChar == '|' || upperCellChar == '7' || upperCellChar == 'F')
+                    outputcells.Add(Tuple.Create(inputCell.Item1 - 1, inputCell.Item2));
+                if (lowerCellChar == '|' || lowerCellChar == 'J' || lowerCellChar == 'L')
+                    outputcells.Add(Tuple.Create(inputCell.Item1 + 1, inputCell.Item2));
                 break;
             default:
                 break;
@@ -73,7 +115,6 @@ public class Scanner10
         Console.WriteLine(lowerLeftCellChar);
         Console.WriteLine(leftCellChar);
         Console.WriteLine(upperLeftCellChar);
-        var outputcells = new List<Tuple<int, int>>();
         return outputcells;
     }
 
@@ -81,9 +122,10 @@ public class Scanner10
     {
         var watch = System.Diagnostics.Stopwatch.StartNew();
         CreatePipemap(data);
-        Console.WriteLine(GetStart());
+        parser.PrintMatrix(pipemap);
+        //Console.WriteLine(GetStart());
         Console.WriteLine("___");
-        GetValidCells(Tuple.Create(0,0));
+        GetValidCells(Tuple.Create(1,3)).ForEach(c => Console.WriteLine(c));
         //GetValidCells(GetStart());
         watch.Stop();
         var elapsedMs = watch.Elapsed.TotalMilliseconds;
